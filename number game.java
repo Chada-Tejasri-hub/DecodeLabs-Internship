00af3c72be139a24e898acb360eece309397ed7a
Chada-Tejasri-hub <tejasrichada640@gmail.com>
2026-06-22 14:21:22 +0530

    Add files via upload

---
+++ number game.java
@@ -0,0 +1,60 @@
+import java.util.Random;
+import java.util.Scanner;   
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Start small. Ship something.");
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+
+        int score = 0;
+        char playAgain;
+
+        System.out.println("===== NUMBER GUESSING GAME =====");
+
+        do {
+            int randomNumber = random.nextInt(100) + 1;
+            int attempts = 0;
+            int maxAttempts = 10;
+            boolean guessedCorrectly = false;
+
+            System.out.println("\nI have selected a number between 1 and 100.");
+            System.out.println("You have " + maxAttempts + " attempts to guess it.");
+
+            while (attempts < maxAttempts) {
+                System.out.print("\n Enter your guess: ");
+                int guess = sc.nextInt();
+                attempts++;
+
+                if (guess == randomNumber) {
+                    System.out.println("Congratulations! You guessed the number.");
+                    System.out.println("Attempts used: " + attempts);
+
+                    score += (maxAttempts - attempts + 1);
+                    guessedCorrectly = true;
+                    break;
+                } else if (guess < randomNumber) {
+                    System.out.println("Too Low! Try again.");
+                } else {
+                    System.out.println("Too High! Try again.");
+                }
+
+                System.out.println(" Remaining Attempts: " + (maxAttempts - attempts));
+            }
+
+            if (!guessedCorrectly) {
+                System.out.println("Game Over!");
+                System.out.println("The correct number was: " + randomNumber);
+            }
+
+            System.out.println("Current Score: " + score);
+            System.out.print("Do you want to play again? (Y/N): ");
+            playAgain = sc.next().charAt(0);
+
+        } while (playAgain == 'Y' || playAgain == 'y');
+
+        System.out.println("Final Score: " + score);
+        System.out.println("Thank you for playing!");
+
+        sc.close();
+    }
+}	
